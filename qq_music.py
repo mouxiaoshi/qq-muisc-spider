a1c46b2b8342794979fe00df1687d6bacaefb1f0
mouxiaoshi <mouxiaoshi@qq.com>
2021-01-21 15:42:23 +0800

    <init> 项目初始化

---
+++ qq_music.py
@@ -0,0 +1,290 @@
+#! /usr/local/bin/python3.8
+# -*- coding: UTF-8 -*-
+
+import requests
+from requests.cookies import RequestsCookieJar
+
+import util
+import random
+import time
+import json
+import os
+import pickle
+
+from urllib import parse
+from PIL import Image
+
+
+class SpiderSession:
+    """session"""
+
+    def __init__(self):
+        self.cookie_path = 'cookies/'
+        self.session = requests.session()
+        # pt数据
+        self.pt = util.get_pt()
+
+    def get_session(self):
+        return self.session
+
+    def get_pt(self):
+        return self.pt
+
+    def get_cookies(self):
+        return self.session.cookies
+
+    def set_cookie(self, cookies):
+        return self.session.cookies.update(cookies)
+
+    def save_cookies_to_local(self, cookie_file_name):
+        """
+        保存Cookie到本地
+        :param cookie_file_name: 存放Cookie的文件名称
+        :return:
+        """
+        path = self.cookie_path
+        filepath = path + cookie_file_name + '.cookie'
+        if not (os.path.exists(path)):
+            os.makedirs(path)
+        with open(filepath, 'wb') as f:
+            pickle.dump(self.get_cookies(), f)
+
+    def load_cookies_from_local(self):
+        """
+        从本地加载Cookie
+        :return:
+        """
+        path = self.cookie_path
+        if os.path.exists(path):
+            for file_name in os.listdir(path):
+                if file_name.endswith('.cookie'):
+                    cookie_file_name = file_name
+                    filepath = path + cookie_file_name
+                    load_cookie = pickle.load(open(filepath, 'rb'))
+                    self.set_cookie(load_cookie)
+
+    def get_user_id(self):
+        return self.get_cookies().get('uin')
+
+
+class Login:
+    """二维码扫码登陆"""
+
+    def __init__(self, spider_session=SpiderSession):
+        self.spider_session = spider_session
+        self.session = self.spider_session.get_session()
+        self.pt = self.spider_session.get_pt()
+
+    # 获取跳转链接
+    def _get_polling_url(self, str_p):
+        c = self.pt
+        t = 'https://ssl.' if c['ptui']['isHttps'] else 'http://'
+        t = t + 'ptlogin2.' + c['ptui']['domain'] + '/' + str_p + '?'
+        t += 'appid=' + c['ptui']['appid'] + '&e=2&l=M&s=3&d=72&v=4&t=' + str(random.random())
+        if c['ptui']['daid']:
+            t += '&daid=' + c['ptui']['daid']
+            # if s.isTim:
+            # t += '&tim=1'
+        if c['ptui']['pt_3rd_aid']:
+            t += '&pt_3rd_aid=' + c['ptui']['pt_3rd_aid']
+        return t
+
+    # 获取ptlogin2的地址
+    def ptlogin2(self):
+        client_id = '100497308'
+        s_url = 'https://graph.qq.com/oauth2.0/login_jump'
+        s_url = 'https://xui.ptlogin2.qq.com/cgi-bin/xlogin?appid=716027609&daid=383&style=33&theme=2&login_text=%E6%8E%88%E6%9D%83%E5%B9%B6%E7%99%BB%E5%BD%95&hide_title_bar=1&hide_border=1&target=self&s_url=' + parse.quote(
+            s_url, safe='')
+        s_url += '&pt_3rd_aid=' + parse.quote(client_id, safe='')
+        dm_host = ''
+        feed_back_link = 'https://support.qq.com/products/77942?customInfo=' + parse.quote(dm_host,
+                                                                                           safe='') + '.appid' + client_id
+        s_url += ('&pt_feedback_link=' + parse.quote(feed_back_link, safe=''))
+        return self.session.get(s_url)
+
+    # 获取二维码登陆图片地址
+    def get_qrlogin_pic(self):
+        heads = {
+        }
+        url = self._get_polling_url('ptqrshow')
+        response = self.session.get(url)
+        util.save_img(response, "login_qr.png")
+
+    def check(self):
+        pt_login_sig = self.session.cookies.get('pt_login_sig')
+        if pt_login_sig is None:
+            pt_login_sig = self.pt['ptui']['login_sig']
+        qrsig = self.session.cookies.get('qrsig')
+        login_url = 'https://ssl.' if self.pt['ptui']['isHttps'] else 'http://'
+        login_url += 'ptlogin2.' + self.pt['ptui']['domain'] + '/'
+        t = 'ptqrlogin'
+        login_url += t + '?'
+        login_url += 'u1=' + parse.quote(self.pt['ptui']['s_url'], safe='')
+        login_url += '&ptqrtoken=' + str(util.get_ptqrtoken(qrsig))
+        login_url += '&ptredirect=' + str(self.pt['ptui']['target'])
+        login_url += '&h=1&t=1&g=1&from_ui=1'
+        login_url += '&ptlang=' + self.pt['ptui']['lang']
+        login_url += '&action=0-0-' + str(int(time.time()))
+        login_url += '&js_ver=' + self.pt['ptui']['ptui_version']
+        login_url += '&js_type=1'
+        login_url += '&login_sig=' + pt_login_sig
+        login_url += '&pt_uistyle=' + self.pt['ptui']['style']
+        login_url += '&aid=' + self.pt['ptui']['appid']
+        login_url += '&daid=' + self.pt['ptui']['daid']
+        login_url += '&pt_3rd_aid=' + self.pt['ptui']['pt_3rd_aid']
+        resp = self.session.get(login_url)
+        return resp
+
+    def authorize(self):
+        c = RequestsCookieJar()
+        ui = util.guid()
+        c.set('ui', ui, path='/', domain='graph.qq.co')
+        self.session.cookies.update(c)
+
+        cgi_url = 'https://graph.qq.com/oauth2.0/authorize'
+
+        headers = {
+            'User-Agent': util.get_user_agents(),
+            'Referer': 'https://graph.qq.com/oauth2.0/show?which=Login&display=pc&response_type=code&client_id=100497308&redirect_uri=https%3A%2F%2Fy.qq.com%2Fportal%2Fwx_redirect.html%3Flogin_type%3D1%26surl%3Dhttps%253A%252F%252Fy.qq.com%252Fportal%252Fprofile.html%2523stat%253Dy_new.top.user_pic%2526stat%253Dy_new.top.pop.logout%26use_customer_cb%3D0&state=state&display=pc',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+
+        payload = {
+            'response_type': 'code',
+            'client_id': '100497308',
+            'redirect_uri': 'https://y.qq.com/portal/wx_redirect.html?login_type=1&surl=https%3A%2F%2Fy.qq.com%2F%23&use_customer_cb=0',
+            'scope': '',
+            'state': 'state',
+            'switch': '',
+            'from_ptlogin': '1',
+            'src': '1',
+            'update_auth': '1',
+            'openapi': '80901010',
+            'g_tk': util.get_g_tk(self.session.cookies.get('p_skey')),
+            'auth_time': str(int(time.time())),
+            'ui': ui
+        }
+        urlencode = parse.urlencode(payload)
+        post = self.session.post(cgi_url, headers=headers, data=urlencode)
+        return post
+
+    # 获取认证的cookie
+    def get_login_cookie(self, code):
+        headers = {
+            'User-Agent': util.get_user_agents(),
+            'Referer': 'https://y.qq.com/',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        g_tk = util.get_g_tk(self.session.cookies.get('p_skey'));
+        param = {
+            "comm": {
+                "g_tk": g_tk,
+                "platform": "yqq",
+                "ct": 24,
+                "cv": 0
+            },
+            "token": {
+                "module": "QQConnectLogin.LoginServer",
+                "method": "QQLogin",
+                "param": {
+                    "code": code
+                }
+            }
+        }
+        response = self.session.post('https://u.y.qq.com/cgi-bin/musicu.fcg', headers=headers, data=json.dumps(param))
+        return response
+
+    # 登陆
+    def login(self):
+        # 获取登陆二维码
+        self.get_qrlogin_pic()
+        print('清扫描二维码登陆')
+        image_open = Image.open(os.getcwd() + '/login_qr.png')
+        image_open.show()
+        while (True):
+            check = self.check()
+            values = check.cookies.values()
+            if len(values) > 0:
+                # 字节转化为字符串
+                s = str(check.content)
+                split = s.split(',')
+                # 跳转返回的url(获取响应头的cookies)
+                self.session.get(split[2].replace('\'', ''))
+                break
+            time.sleep(3)
+        # 登陆认证获取cookie
+        authorize = self.authorize()
+        code = util.get_code(authorize.url)
+        self.get_login_cookie(code)
+        self.spider_session.save_cookies_to_local(self.spider_session.get_user_id())
+
+
+class QqMusicUtil(object):
+    """qq music工具包"""
+
+    def __init__(self):
+        self.spider_session = SpiderSession()
+        # self.spider_session = spider_session
+        self.spider_session.load_cookies_from_local()
+        self.session = self.spider_session.session
+
+    # 检测是否登录
+    def check_login(self):
+        data = {"comm":
+                    {"ct": 24, "cv": 0},
+                "getFavorList": {"method": "get_favor_list",
+                                 "param":
+                                     {"userid": self.spider_session.get_user_id(),
+                                      "fav_type": 1},
+                                 "module": "music.favor_system_read"}
+                }
+        dumps = json.dumps(data)
+        payload = {
+            'sign': util.get_sign(dumps),
+            'data': dumps,
+        }
+        response = self.session.get('https://u.y.qq.com/cgi-bin/musics.fcg', params=payload)
+        return response
+
+
+if __name__ == '__main__':
+    # print(get_pt()['str'])
+    # url = "https://xui.ptlogin2.qq.com/cgi-bin/xlogin?appid=716027609&daid=383&style=33&theme=2&login_text=%E6%8E%88%E6%9D%83%E5%B9%B6%E7%99%BB%E5%BD%95&hide_title_bar=1&hide_border=1&target=self&s_url=https%3A%2F%2Fgraph.qq.com%2Foauth2.0%2Flogin_jump&pt_3rd_aid=100497308&pt_feedback_link=https%3A%2F%2Fsupport.qq.com%2Fproducts%2F77942%3FcustomInfo%3D.appid100497308"
+    # print(util.ptlogin2() == url)
+    # get = requests.get(url)
+    # print(get.cookies)
+
+    spider_session = SpiderSession()
+
+    login = Login(spider_session)
+
+    login.login()
+
+    # login.ptlogin2()
+    # login.get_qrlogin_pic()
+    # while (True):
+    #     check = login.check()
+    #     values = check.cookies.values()
+    #     print(len(values))
+    #     if len(values) > 0:
+    #         # 字节转化为字符串
+    #         s = str(check.content)
+    #         split = s.split(',')
+    #         get = login.session.get(split[2].replace('\'', ''))
+    #         print(get)
+    #         print(s)
+    #         break
+    #     time.sleep(3)
+    # authorize = login.authorize()
+    # code = util.get_code(authorize.url)
+    # response = login.get_login_cookie(code)
+    # spider_session.save_cookies_to_local(spider_session.get_user_id())
+    # for cookie in response.cookies:
+    #     print(cookie.name + "=" + cookie.value + ";")
+
+    # print(util.get_sign(
+    #     '{"comm":{"ct":24,"cv":0},"getFavorList":{"method":"get_favor_list","param":{"userid":"10933495","fav_type":1},"module":"music.favor_system_read"}}'))
+    # pass
+    music_util = QqMusicUtil()
+    check_login = music_util.check_login()
+    print(check_login.content)
---
+++ util.py
@@ -0,0 +1,142 @@
+#! /usr/local/bin/python3.8
+# -*- coding: UTF-8 -*-
+
+import random
+import execjs
+import re
+
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2226.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
+    "Mozilla/5.0 (X11; OpenBSD i386) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2309.372 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2117.157 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1866.237 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/4E423F",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36 Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1664.3 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1623.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.17 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36",
+    "Mozilla/5.0 (X11; CrOS i686 4319.74.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.2 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1467.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1464.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1500.55 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.90 Safari/537.36",
+    "Mozilla/5.0 (X11; NetBSD) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36",
+    "Mozilla/5.0 (X11; CrOS i686 3912.101.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.15 (KHTML, like Gecko) Chrome/24.0.1295.0 Safari/537.15",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.14 (KHTML, like Gecko) Chrome/24.0.1292.0 Safari/537.14"
+]
+
+
+# 获取浏览器认证头
+def get_user_agents():
+    return random.choice(USER_AGENTS)
+
+
+# 读取js
+def load_js(name):
+    f = open(name, 'r', encoding='utf-8')
+    js_text = ''
+    while True:
+        readline = f.readline()
+        if readline:
+            js_text += readline
+        else:
+            break
+    return js_text
+
+
+# 读取sign.js
+def get_sign_js():
+    return load_js('sign.js')
+
+
+# pt_js.js
+def get_pt_js():
+    return load_js('pt_data.js')
+
+
+# others.js
+def get_others_js():
+    return load_js('others.js')
+
+
+# 获取签名
+def get_sign(requestDate):
+    # 加载js
+    execjs_execjs = execjs.compile(get_sign_js())
+    return execjs_execjs.call('getSign', requestDate)
+
+
+# 获取pt
+def get_pt():
+    # 加载js
+    execjs_execjs = execjs.compile(get_pt_js())
+    return execjs_execjs.call('getPt', None)
+
+
+# 获取ptqrtoken
+def get_ptqrtoken(qrsign):
+    # 加载js
+    execjs_execjs = execjs.compile(get_others_js())
+    return execjs_execjs.call('hash33', qrsign)
+
+
+# 获取g_tk
+def get_g_tk(p_skey):
+    # 加载js
+    execjs_execjs = execjs.compile(get_others_js())
+    return execjs_execjs.call('getToken', p_skey)
+
+
+# generate golobally unique identifier
+def guid():
+    # 加载js
+    execjs_execjs = execjs.compile(get_others_js())
+    return execjs_execjs.call('guid')
+
+
+# 保存图片到本地
+def save_img(response, img_file):
+    with open(img_file, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=1024):
+            f.write(chunk)
+
+
+# 查找返回url中的code
+def get_code(url):
+    pattern = re.compile(r'[A-Za-z0-9]{16,}')  # 查找
+    result = pattern.findall(url)
+    return result[0]
+
